d67c0e7efb55653d22ee10c234330a1eebb9f114
unknown <tim_critt@hotmail.com>
2025-09-02 22:31:54 +0200

    Minor CSS changes to improve mobile view and layout

--- src/components/TestWrapper/TestWrapper.jsx
+++ src/components/TestWrapper/TestWrapper.jsx
@@ -3,7 +3,9 @@ import styles from "./TestWrapper.module.css";
 //Higher order component that wraps the test content with a specific style
 const TestWrapper = ({ children }) => {
 	return (
-		<article className={styles.test_wrapper_container}>{children}</article>
+		<article className={styles.test_wrapper_container + " mobile_side_margin"}>
+			{children}
+		</article>
 	);
 };
 
